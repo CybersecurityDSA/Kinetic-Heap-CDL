2cfe4d32c4d13ebe7ec4e3e96144382b96b61687
Rosetejo18 <166843026+Rosetejo18@users.noreply.github.com>
2024-04-25 20:21:15 +0530

    Update Circular with delete function (Haresh's code)

--- Circular.cpp
+++ Circular.cpp
@@ -65,7 +65,67 @@ public:
 }
     
     //Deletion
+    void Delf() {
+    	if (head == nullptr) {
+    		cout << "Null node present" << endl;
+    		return;
+		}
 
+        Node* Del = head->next;
+        if (head->next == head) 
+            head = nullptr;
+		else {
+            head->prev = Del->prev;
+            Del->prev->next = head;
+        }
+        delete Del;
+	}
+	
+	void Delb() {
+		if (head == nullptr) {
+    		cout << "Null node present" << endl;
+    		return;
+		}
+        Node* Del = head->prev;
+        if (head->next == head) 
+            head = nullptr;
+        else {
+            head->prev = Del->prev;
+            Del->prev->next = head;
+        }
+        delete Del;
+	}
+	
+	void Deln() {
+		if (head == nullptr) {
+    		cout << "Null node present" << endl;
+    		return;
+		}
+		int d;
+        cout << "Enter the value of the node to detele:";
+        cin >> d;
+        
+		Node* current = head;
+        do {
+            if (current->data == d) {
+                if (current == head) {
+                    Delf();
+                    return;
+                } 
+				else if (current->next == head) {
+                    Delb();
+                    return;
+                } else {
+                    current->prev->next = current->next;
+                    current->next->prev = current->prev;
+                    delete current;
+                    return;
+                }
+            }
+            current = current->next;
+        } while (current != head);
+	}
+      
     // Traversal
     void traverseForward() {
         if (head==nullptr)
@@ -139,37 +199,40 @@ int main() {
     Node* node;
 
     do {
-        cout << "Circular Doubly Linked List Operations:" << endl;
-        cout << "3. Insert with key"
-        cout << "5. Insert at Beginning" << endl;
-        cout << "8. Traverse Forward" << endl;
-        cout << "9. Traverse Backward" << endl;
-        cout << "10.Search" << endl;
-        cout << "11. Check Empty " << endl;
-        cout << "12. Size" << endl;
-        cout << "13. Reverse" << endl;
-        cout << "14. Clear" << endl;
+        cout << "Circular Doubly Linked List Operations:" << endl
+        cout << "1. Insert at Beginning" << endl;
+        cout << "2. Insert with key"<<endl
+        cout << "3. Traverse Forward" << endl;
+        cout << "4. Traverse Backward" << endl;
+        cout << "5. Search" << endl;
+        court<<"6. Delete front"<<endl
+        cout<< "7. Delete back"<<endl
+        cout<< "8. Delete Node"<<endl  
+        cout << "9. Check Empty " << endl;
+        cout << "10. Size" << endl;
+        cout << "11. Reverse" << endl;
+        cout << "12. Clear" << endl;
         cout << "0. Exit" << endl;
         cout << "Enter your choice: ";
         cin >> choice;
 
         switch (choice) {
-            case 3:
+            case 2:
                 cin
                 List.insertAtKey(int key, int data)
-            case 5:
+            case 1:
                 int data;
                 cout << "Enter data to insert at the beginning: ";
                 cin >> data;
                 List.insertBegin(data);
                 break;
-            case 8:
+            case 3:
                 List.traverseForward();
                 break;
-            case 9:
+            case 4:
                 List.traverseBackward();
                 break;
-            case 10:
+            case 5:
                 int key;
                 cout << "Enter the key to search: ";
                 cin >> key;
@@ -179,16 +242,25 @@ int main() {
                 else
                     cout << "Key not found in the list." << endl;
                 break;
-             case 11:
+            case 6:
+              List.Delf();
+              break
+            case 7:
+              List.Delb();
+              break
+            case 8:
+              List.Deln();
+              break
+            case 9:
                 List.isEmpty();
                 break;
-            case 12:
+            case 10:
                 List.size();
                 break;
-            case 13:
+            case 11:
                 List.reverse();
                 break;
-            case 14:
+            case 12:
                 List.clear();
                 break;
             case 0:
